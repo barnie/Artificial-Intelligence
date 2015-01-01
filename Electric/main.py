3e19e87e821a6655488e280775382ee8e8913382
Piotr Kruk <piotr@kruk.co>
2015-01-01 20:18:31 +0100

    refactore

--- Electric/main.py
+++ Electric/main.py
@@ -1,16 +1,10 @@
 __author__ = 'piotr'
 
 from data import electric_handler as el
-from Neuron import Network
-import neurolab as nl
-from ffnet import *
-import networkx as NX
-import pylab as pl
-import math
+from ffnet import tmlgraph, ffnet
 import copy
-from numpy import linspace
 from data import weather_city
-from theano import *
+
 
 # Wejscie: a) p(-2), p(-1) - energia z ostatnich dwoch godzin tego dnia,
 # b) p(-26), p(-25) - analogicznie ale z dnia poprzedniego,
@@ -18,39 +12,45 @@ from theano import *
 # d) dzien roku analogicznie jak dzien tygodnia,
 # e) dane meteorologiczne dla 9 miast (punktow): temperatura, cisnienie,
 # wilgotnosc powietrza, opady.
-global e
-e = el.electric_hander()
+global ELECTRIC
+ELECTRIC = el.electric_hander()
 
-bialystok = weather_city.weather_cit('bialystok')
-gorzow = weather_city.weather_cit('gorzow')
-katowice = weather_city.weather_cit('katowice')
-kielce = weather_city.weather_cit('kielce')
-krakow = weather_city.weather_cit('krakow')
-lublin = weather_city.weather_cit('lublin')
-olsztyn = weather_city.weather_cit('olsztyn')
+BIALYSTOK = weather_city.weather_cit('bialystok')
+GORZOW = weather_city.weather_cit('gorzow')
+KATOWICE = weather_city.weather_cit('katowice')
+KIELCE = weather_city.weather_cit('kielce')
+KRAKOW = weather_city.weather_cit('krakow')
+LUBLIN = weather_city.weather_cit('lublin')
+OLSZTYN = weather_city.weather_cit('olsztyn')
 
-meteo = [bialystok.getWeather(), gorzow.getWeather(), katowice.getWeather(), kielce.getWeather(),
-         krakow.getWeather(), lublin.getWeather(), olsztyn.getWeather()]
+LEARNING_COMMITTEE = []
+TESTING_COMMITTEE = []
 
-# at now only for one hour
+NETWORKS = []
 
-def prepareInput():
-    dane = copy.copy(e.getElectricDays())
+METEO = [BIALYSTOK.getWeather(), GORZOW.getWeather(), KATOWICE.getWeather(),
+ KIELCE.getWeather(), KRAKOW.getWeather(), LUBLIN.getWeather(), OLSZTYN.getWeather()]
+
+
+def prepare_input():
+    """ Prepare input to neuronal network """
+    dane = copy.copy(ELECTRIC.getElectricDays())
     for i in range(len(dane)):
         dane[i].getHourList().append(dane[i].getDate().timetuple().tm_yday)
-        for j in meteo:
+        for j in METEO:
             if i < 1460:
                 dane[i].getHourList().append(j[i][3])
                 dane[i].getHourList().append(j[i][4])
                 dane[i].getHourList().append(j[i][5])
                 dane[i].getHourList().append(j[i][6])
-    input = []
+    input1 = []
     for i in dane:
-        input.append(i.getHourList())
-    return input[:-1]
+        input1.append(i.getHourList())
+    return input1[:-1]
 
 
-def prepareTarget():
+def prepare_target():
+    """Prepare target to compare if output from network is good"""
     target = []
     dane = el.electric_hander().getElectricDays()
     i = 0
@@ -61,22 +61,34 @@ def prepareTarget():
     return target
 
 
-target = prepareTarget()
-input = prepareInput()
+TARGET = prepare_target()
+INPUT = prepare_input()
+
+
+def prepare_data():
+    """Preparing data for specific neuronal networks problem"""
+    pass
+
+def calculate_network(input_, target_, test_input, test_output, net1):
+    """" Return learned network and, outpur,regression for test """
+    network = net1.train_rprop(input_, target_, mimin=1e-06, mimax=500.0,
+        xmi=0.2, maxiter=1, disp=1)
+    output, regression = net1.test(test_input, test_output, iprint=2)
+    return network, output, regression
+
 #
-conec = tmlgraph((53, 45, 45, 24))  # tmlgraph or imlgraph? which one will be better ?
-net = ffnet(conec)
-print len(input), len(target)
+CONEC = tmlgraph((53, 45, 45, 24))  # tmlgraph or imlgraph? which one will be better ?
+NET = ffnet(CONEC)
+print len(INPUT), len(TARGET)
 # NX.draw_graphviz(net.graph, prog='dot') show the network that's nice!
 # pl.show()
-dd = net.train_rprop(input[:-365], target[:-365], mimin=1e-06, mimax=500.0, xmi=0.2, maxiter=10000, disp=1)
-output, regression = net.test(input[-365:], target[-365:], iprint=2)
 
-sum = 0.0
+calculate_network(INPUT[:-365], TARGET[:-365], INPUT[-365:], TARGET[-365:], NET)
 
-for i in range(len(output)):
-    for j in range((len(output[i]))):
-        sum = sum + ((abs(float(target[i][j]) - float(output[i][j]))) / float(output[i][j]))
-print sum / 365
+# sum = 0.0
+# for i in range(len(output)):
+#     for j in range((len(output[i]))):
+#         sum = sum + ((abs(float(target[i][j]) - float(output[i][j]))) / float(output[i][j]))
+# print sum / 365
 
-print len(bialystok.getWeather()), len(prepareInput())
+# print len(bialystok.getWeather()), len(prepareInput())
